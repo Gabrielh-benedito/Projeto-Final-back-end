c15d6b6bc02dcd63930f274868a8a4d9391edc54
Gabriel Benedito <gabrielh.beneditoo>
2025-11-14 10:30:07 -0300

    Rota listar produtos ✔

--- back-end/main.py
+++ back-end/main.py
@@ -13,4 +13,19 @@ def cadastrar_produtos(nome: str, categoria: str, preco: float, quantidade: floa
     funcao.adicionar_produto(nome, categoria, preco, quantidade)
     return{"200": "Produto cadastrado com sucesso!"}
 
+@app.get("/produtos")
+def listar_produtos():
+    produtos = funcao.listar_produtos()
+    lista = []
+    for linha in produtos:
+        lista.append(
+            {
+               "id": linha [0],
+               "nome": linha [1],
+               "categoria": linha [2],
+               "preco": linha [3],
+               "quantidade": linha [4]
+            }
+        )
+    return {"produtos:": lista}
 
